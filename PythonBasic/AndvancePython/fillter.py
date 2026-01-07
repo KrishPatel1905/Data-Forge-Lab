bfc544099279a3bd81a226a334a8e89de670996d
Krish Patel <krishhpatel1905@gmail.com>
2026-01-07 17:45:46 +0530

    Python Added

--- PythonBasic/AndvancePython/fillter.py
+++ PythonBasic/AndvancePython/fillter.py
@@ -1,12 +1,12 @@
-def check(x):
-    if x>18:
-        return True
-    else:
-        return False
+# def check(x):
+#     if x>18:
+#         return True
+#     else:
+#         return False
     
 a = [15,0,45,787,1,15,16,19]
 
-new = list(filter(check,a))
+new = list(filter(lambda x :x>18,a))
 
 print(new)
 
